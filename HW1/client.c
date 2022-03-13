4faf0ac76fe0d0e475dadc509a78f3a6e8bb7da4
bosco0127 <bosco0127@kaist.ac.kr>
2022-03-13 14:07:49 +0900

    client_packet structure

--- HW1/client.c
+++ HW1/client.c
@@ -1,35 +1,87 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<arpa/inet.h>
-#include<sys/socket.h>
-#include<unistd.h>
-#include<string.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+typedef struct packet {
+    unsigned short op;
+    unsigned short shift;
+    unsigned int length;
+    char string[4];
+} Packet;
 
 int main(int argc, char* argv[])
 {
-    int my_sock;
-    struct sockaddr_in serv_addr;
-    int str_len;
+    // 0. Declare variables
+    // client file/socket descriptor
+    int client_socket;
+    // server socket address
+    struct sockaddr_in server_address;
+    // Packet
+    Packet p_write;
+    Packet p_read;
+    char write_message[11]={0x00,0x00,0x01,0x00,
+    0x00,0x00,0x00,0x0B,
+    0x61,0x62,0x63};
+    char read_message[15];
+    // packet length
+    int write_len;
+    int read_len;
+
     if(argc != 3)
     {
         printf("%s <IP> <PORT>\n", argv[0]);
         exit(1);
     }
-    my_sock = socket(PF_INET,SOCK_STREAM,0); //1번
-    if(my_sock == -1)
-        printf("socket error \n");
-    memset(&serv_addr,0,sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
-    serv_addr.sin_port=htons(atoi(argv[2]));
 
-    if(connect(my_sock,(struct sockaddr*)&serv_addr,sizeof(serv_addr))==-1) //2번
-        printf("connect error\n");
-    char message[15];
-    str_len = read(my_sock,message,sizeof(message)-1); //3번
-    if(str_len==-1)
-        printf("read error\n");
-    printf("서버에서 : %s \n", message);
-    close(my_sock); //4번
+    // for test
+    // initialize Packet
+    p_write.op = 0;
+    p_write.shift= 1;
+    p_write.length=htonl(sizeof(p_write));
+    strcpy(p_write.string, "abc");
+
+    // 1. socket
+    // creates socket with error handling.
+    client_socket = socket(PF_INET,SOCK_STREAM,0);
+    if(client_socket == -1) {
+        fprintf(stderr,"socket() error\n");
+        assert(0);
+    }
+
+    // 2. connect
+    // clean server_address
+    memset(&server_address,0,sizeof(server_address));
+    // set server_address
+    server_address.sin_family = AF_INET; // IPv4
+    server_address.sin_addr.s_addr=inet_addr(argv[1]); // IP address
+    server_address.sin_port=htons(atoi(argv[2])); // Port no.
+    // connect with error handling.
+    if(connect(client_socket,(struct sockaddr*) &server_address, sizeof(server_address))==-1) {
+        fprintf(stderr,"connect() error\n");
+        assert(0);
+    }
+
+    // 3. write
+    write_len = write(client_socket, &p_write, sizeof(p_write));
+    if(write_len==-1) {
+        fprintf(stderr,"send() error\n");
+        assert(0);
+    }
+
+    // 4. read
+    read_len = read(client_socket, &p_read, sizeof(p_read));
+    if(read_len==-1) {
+        fprintf(stderr,"recv() error\n");
+        assert(0);
+    }
+    // print result to stdout
+    printf("server : %s \n", p_read.string);
+    
+    // 5. close
+    close(client_socket);
     return 0;
 }
