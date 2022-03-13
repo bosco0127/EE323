fd9998f4613e1ee2fc5709f641270f72a2f24c76
bosco0127 <bosco0127@kaist.ac.kr>
2022-03-13 15:17:05 +0900

    client_option done

--- HW1/client.c
+++ HW1/client.c
@@ -23,27 +23,57 @@ int main(int argc, char* argv[])
     // Packet
     Packet p_write;
     Packet p_read;
-    char write_message[11]={0x00,0x00,0x01,0x00,
+    /* char write_message[11]={0x00,0x00,0x01,0x00,
     0x00,0x00,0x00,0x0B,
     0x61,0x62,0x63};
-    char read_message[15];
+    char read_message[15]; */
     // packet length
     int write_len;
     int read_len;
+    // option variable
+    char opt;
 
-    if(argc != 3)
-    {
-        printf("%s <IP> <PORT>\n", argv[0]);
-        exit(1);
+    // 0-1 Separate options
+    // check if all options are involved.
+    if(argc != 9) {
+        fprintf(stderr,"need to contain -h <host> -p <port> -o <option> -s <shift> options\n");
+        assert(0);
+    }
+    // clean server_address
+    memset(&server_address,0,sizeof(server_address));
+    // set server_address
+    server_address.sin_family = AF_INET; // IPv4
+    while( (opt = getopt(argc,argv,"h:p:o:s:")) != -1 ) {
+        switch(opt) {
+            case 'h':
+                server_address.sin_addr.s_addr=inet_addr(optarg); // IP address
+                break;
+            case 'p':
+                server_address.sin_port=htons(atoi(optarg)); // Port no.
+                break;
+            case 'o':
+                p_write.op = (short) atoi(optarg); // op type.
+                break;
+            case 's':
+                p_write.shift = (short) atoi(optarg); // shift number.
+                break;
+            case '?':
+                fprintf(stderr,"only -h -p -o -s is allowed for options\n");
+                assert(0);
+                break;
+            default:
+                fprintf(stderr,"only -h -p -o -s is allowed for options\n");
+                assert(0);
+                break;
+        }
+
     }
 
     // for test
     // initialize Packet
-    p_write.op = 0;
-    p_write.shift= 1;
     p_write.length=htonl(sizeof(p_write));
     strcpy(p_write.string, "abc");
-
+    
     // 1. socket
     // creates socket with error handling.
     client_socket = socket(PF_INET,SOCK_STREAM,0);
@@ -53,12 +83,7 @@ int main(int argc, char* argv[])
     }
 
     // 2. connect
-    // clean server_address
-    memset(&server_address,0,sizeof(server_address));
-    // set server_address
-    server_address.sin_family = AF_INET; // IPv4
-    server_address.sin_addr.s_addr=inet_addr(argv[1]); // IP address
-    server_address.sin_port=htons(atoi(argv[2])); // Port no.
+    
     // connect with error handling.
     if(connect(client_socket,(struct sockaddr*) &server_address, sizeof(server_address))==-1) {
         fprintf(stderr,"connect() error\n");
